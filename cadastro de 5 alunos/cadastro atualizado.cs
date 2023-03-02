528b6b40b3aa3152a2a941c265d58e475bb5c0a4
JOAO PAULO MOREIRA SILVA <joao.pmsilva47@senacsp.edu.br>
2023-03-02 19:46:00 -0300

    joao

---
+++ cadastro de 5 alunos/cadastro atualizado.cs
@@ -0,0 +1,28 @@
+﻿// Criar Variáveis (Marina)
+string[,] alunos = new string[5, 7];
+int linha = 0;
+int coluna = 0;
+int codigo = 0;
+string continuar = "S";
+double total = 0;
+
+// Cadastrar Produtos
+for (linha = 0; linha <= 5; linha++)
+{
+    Console.WriteLine("Informe seu nome: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe o numero de auals dadas: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe o numero de faltas: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 1: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 2: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 3: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota do trabalho 1: ");
+    alunos[linha, coluna++] = Console.ReadLine();
+
+    coluna = 0;
+}
---
+++ cadastro de 5 alunos/cadastro de 5 alunos.cs
@@ -0,0 +1,28 @@
+﻿// Criar Variáveis (Marina)
+string[,] cadastro = new string[5, 7];
+int linha = 0;
+int coluna = 0;
+int codigo = 0;
+string continuar = "S";
+double total = 0;
+
+// Cadastrar Produtos
+for (linha = 0; linha <= 5; linha++)
+{
+    Console.WriteLine("Informe seu nome: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe o numero de auals dadas: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe o numero de faltas: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 1: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 2: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota da prova 3: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+    Console.WriteLine("Informe a nota do trabalho 1: ");
+    cadastro[linha, coluna++] = Console.ReadLine();
+
+    coluna = 0;
+}
